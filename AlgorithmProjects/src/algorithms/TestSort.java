0cb39605bcead9f728d33ceadef5a3cd8a532104
paritoshdave <paritosh.sirohi@gmail.com>
2015-10-29 09:49:04 +0530

    Merge sort implementation.. this can be optimized to reduce value comparision

--- AlgorithmProjects/src/algorithms/TestSort.java
+++ AlgorithmProjects/src/algorithms/TestSort.java
@@ -49,6 +49,38 @@ class TestSort
         result = InsertionSort.sort(arr1, InsertionSort.SORT_TYPE.DESC);
         System.out.println("Printing is desc order");
         printArr(result);
+
+        arr1 = new int[]{1,2,3,4,5};
+        result = MergeSort.sort(arr1);
+        System.out.println("Printing is asc order");
+        printArr(result);
+
+        arr1 = new int[]{10,9,23,1};
+        result = MergeSort.sort(arr1);
+        System.out.println("Printing is asc order");
+        printArr(result);
+
+        arr1 = new int[]{10};
+        result = MergeSort.sort(arr1);
+        System.out.println("Printing is asc order");
+        printArr(result);
+
+
+        arr1 = new int[]{4,3,123,45,2,-5,-1,0,4,12,123,8};
+        result = MergeSort.sort(arr1);
+        System.out.println("Printing is asc order");
+        printArr(result);
+
+        arr1 = new int[]{1,6,3,9,12,8,-17};
+        result = MergeSort.sort(arr1);
+        System.out.println("Printing is asc order");
+        printArr(result);
+
+        arr1 = new int[]{15,2,4,12,4,3,2,1};
+        result = MergeSort.sort(arr1);
+        System.out.println("Printing is asc order");
+        printArr(result);
+
     }
 
     private static void printArr(int[] arr)
